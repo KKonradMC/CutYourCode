bb320bac8b535722233cf6636442ea774cf9172d
KonradMC <konrad.krakowiak1987@gmail.com>
2014-10-11 13:32:09 +0200

    Add @EqualsAndHashCode

--- app/src/main/java/org/kkonrad/cutyourcode/model/ExampleModel.java
+++ app/src/main/java/org/kkonrad/cutyourcode/model/ExampleModel.java
@@ -2,6 +2,7 @@ package org.kkonrad.cutyourcode.model;
 
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.experimental.Accessors;
 @Accessors(prefix = "m")
 @NoArgsConstructor
 @AllArgsConstructor(suppressConstructorProperties = true)
+@EqualsAndHashCode
 public class ExampleModel {
 
     @Getter
@@ -23,27 +25,4 @@ public class ExampleModel {
     @Getter
     @Setter
     private long mThirdProperty;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ExampleModel that = (ExampleModel) o;
-
-        if (mSecondProperty != that.mSecondProperty) return false;
-        if (mThirdProperty != that.mThirdProperty) return false;
-        if (mFirstProperty != null ? !mFirstProperty.equals(that.mFirstProperty) : that.mFirstProperty != null)
-            return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = mFirstProperty != null ? mFirstProperty.hashCode() : 0;
-        result = 31 * result + mSecondProperty;
-        result = 31 * result + (int) (mThirdProperty ^ (mThirdProperty >>> 32));
-        return result;
-    }
 }
